504d3ea6d74b37825f421218971354aa921eedeb
chensuiyi <bimostyle@qq.com>
2020-10-18 23:09:18 +0800

    重新整理工具结构和文件

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const download = require("download-git-repo");
 const fs = require("fs-extra");
 const path = require("path");
 const tempDir = path.resolve(process.cwd(), "temp");
-function _download() {
-    return new Promise((resole, reject) => {
+const initDir = path.resolve(process.cwd());
+// 下载项目
+async function downloadProject() {
+    return new Promise((resolve, reject) => {
         download("https://gitee.com:banshiweichen/yipack#master", tempDir, { clone: true }, function (err) {
             if (err) {
                 reject(err);
             } else {
-                fs.copySync(tempDir, path.resolve(tempDir, ".."), { overwrite: true });
-                fs.removeSync(tempDir);
-                console.log("yipack下载成功");
-                resole();
+                resolve();
             }
         });
     });
 }
 
-async function execute() {
-    fs.removeSync(tempDir);
-    fs.ensureDirSync(tempDir);
-    await _download();
+async function init() {
+    try {
+        fs.removeSync(tempDir);
+        fs.ensureDirSync(tempDir);
+        await downloadProject();
+        fs.copySync(tempDir, initDir, { overwrite: true });
+        fs.removeSync(tempDir);
+        console.log("易打包下载成功");
+    } catch (err) {
+        console.log("易打包下载失败");
+        console.log(err);
+    }
 }
-execute();
+init();
