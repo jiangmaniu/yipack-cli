2840ebfc1959568fb4bd3ef36a7deb2aa320da53
chensuiyi <bimostyle@qq.com>
2020-12-14 23:01:38 +0800

    完善配置

--- .yipack/webpack.config.dev.js
+++ .yipack/webpack.config.dev.js
@@ -8,13 +8,7 @@ let currentConfig = {
     // 开发环境开启缓存
     cache: true,
     devtool: "eval-source-map",
-    watch: true,
-    // 监听文件改动，增量编译
-    watchOptions: {
-        aggregateTimeout: 500,
-        poll: 1000,
-        ignored: /node_modules/,
-    },
+
     optimization: {
         moduleIds: "named",
         chunkIds: "named",
@@ -24,14 +18,13 @@ let currentConfig = {
         removeEmptyChunks: false,
         splitChunks: false,
     },
-    // 限制并行处理的模块数量。可以用于调优性能或获取更可靠的性能分析结果
-    // parallelism: 100,
-    // profile: true,
-    // watchOptions: {
-    //     poll: 1000,
-    //     aggregateTimeout: 600,
-    //     ignored: /node_modules|\.cache/,
-    // },
+    watch: true,
+    // 监听文件改动，增量编译
+    watchOptions: {
+        aggregateTimeout: 500,
+        poll: 1000,
+        ignored: /node_modules/,
+    },
     plugins: [
         //
         new Webpack.HotModuleReplacementPlugin(),
