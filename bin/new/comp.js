ee311c3633b09ee94510aa255e29c9939447af82
chensuiyi <chenbimo@gmail.com>
2021-02-21 21:25:38 +0800

    feat: 创建命令完善

--- bin/new/comp.js
+++ bin/new/comp.js
@@ -1,25 +1,36 @@
+// TODO: 增加多级目录组件
+// 案例：yipack new --comp a/b/c.vue 创建多级目录组件
 // 自带模块
-let path = require("path");
+let path = require('path');
 // 第三方模块
-let _ = require("lodash");
-let fs = require("fs-extra");
-let chalk = require("chalk");
-let ora = require("ora");
-let tool = require("../tool.js");
+let _ = require('lodash');
+let fs = require('fs-extra');
+let chalk = require('chalk');
+let ora = require('ora');
+let tool = require('../tool.js');
 // 配置相关
-let myConfig = require("../../.yipack/webpack.config.my.js");
-let yipackPackage = require("../../package.json");
-let yipackConfig = require("../../.yipack/yipack.config.js");
+let myConfig = require('../../.yipack/webpack.config.my.js');
+let yipackPackage = require('../../package.json');
+let yipackConfig = require('../../.yipack/yipack.config.js');
 module.exports = async function newComp(cmd) {
     let spinner = ora();
     let compNames = tool.getNames(cmd.comp);
-    // 创建组件
-    let htmlFilePath = path.join(myConfig.srcDir, "comps", compNames.camelCaseName + ".vue");
-    if (fs.existsSync(htmlFilePath) === false) {
-        let htmlFileData = _.template(require("../../.yipack/template/compHtml.js"))(compNames);
-        fs.outputFileSync(htmlFilePath, htmlFileData);
-        spinner.succeed(chalk.green("组件创建成功"));
+    // 全局组件目录
+    let compDirectory = path.join(myConfig.srcDir, 'comps', compNames.camelCaseName);
+    if (fs.existsSync(compDirectory) === false) {
+        fs.ensureDirSync(compDirectory);
+        // 创建全局组件
+        let compFilePath = path.join(compDirectory, 'index.vue');
+        let compFileData = _.template(require('../../.yipack/template/compHtml.js'))(compNames);
+        fs.outputFileSync(compFilePath, compFileData);
+        spinner.succeed(chalk.green('全局组件创建成功'));
+
+        // 创建全局组件说明
+        let readmeFilePath = path.join(compDirectory, 'readme.md');
+        let readmeFileData = _.template(require('../../.yipack/template/readme.js'))(compNames);
+        fs.outputFileSync(readmeFilePath, readmeFileData);
+        spinner.succeed(chalk.green('全局组件说明创建成功'));
     } else {
-        spinner.warn(chalk.green("组件已存在"));
+        spinner.warn(chalk.green('组件目录已存在'));
     }
 };
--- bin/new/page.js
+++ bin/new/page.js
@@ -24,18 +24,25 @@ module.exports = async function newPage(cmd) {
         let htmlFilePath = path.join(pageDirPath, 'index.vue');
         let htmlFileData = _.template(require('../../.yipack/template/pageHtml.js'))(rootNames);
         fs.outputFileSync(htmlFilePath, htmlFileData);
+        spinner.succeed(chalk.green('页面创建成功'));
 
-        // 创建路由
+        // 创建页面说明
+        let readmeFilePath = path.join(pageDirPath, 'readme.md');
+        let readmeFileData = _.template(require('../../.yipack/template/readme.js'))(rootNames);
+        fs.outputFileSync(readmeFilePath, readmeFileData);
+        spinner.succeed(chalk.green('页面说明创建成功'));
+
+        // 创建页面路由
         let routeFilePath = path.join(pageDirPath, 'route.js');
         let routeFileData = _.template(require('../../.yipack/template/pageRoute.js'))(rootNames);
         fs.outputFileSync(routeFilePath, routeFileData);
+        spinner.succeed(chalk.green('页面路由创建成功'));
 
-        // 创建api
+        // 创建页面接口
         let apiFilePath = path.join(pageDirPath, 'api.js');
         let apiFileData = _.template(require('../../.yipack/template/pageApi.js'))(rootNames);
         fs.outputFileSync(apiFilePath, apiFileData);
-
-        spinner.succeed(chalk.green('页面创建成功'));
+        spinner.succeed(chalk.green('页面接口创建成功'));
     } else {
         spinner.warn(chalk.green('页面已存在'));
     }
@@ -48,18 +55,32 @@ module.exports = async function newPage(cmd) {
         }
         let subNames = tool.getNames(cmd.subPage);
         let names = tool.getAllNames(rootNames, subNames, compNames);
+        let currentDirectory = path.join(pageDirPath, 'subPages', subNames.camelCaseName);
         // 创建二级页面
-        let htmlFilePath = path.join(pageDirPath, 'subPages', subNames.camelCaseName, 'index.vue');
+        let htmlFilePath = path.join(currentDirectory, 'index.vue');
         if (fs.existsSync(htmlFilePath) === false) {
+            // 创建子页面
             let htmlFileData = _.template(require('../../.yipack/template/subPageHtml.js'))(names);
             fs.outputFileSync(htmlFilePath, htmlFileData);
-
             spinner.succeed(chalk.green('子页面创建成功'));
-            // 创建二级路由
-            let routeFilePath = path.join(pageDirPath, 'subPages', subNames.camelCaseName, 'routePage.js');
+
+            // 创建子页面说明
+            let readmeFilePath = path.join(currentDirectory, 'readme.md');
+            let readmeFileData = _.template(require('../../.yipack/template/readme.js'))(rootNames);
+            fs.outputFileSync(readmeFilePath, readmeFileData);
+            spinner.succeed(chalk.green('子页面说明创建成功'));
+
+            // 创建子页面路由
+            let routeFilePath = path.join(currentDirectory, 'routePage.js');
             let routeFileData = _.template(require('../../.yipack/template/subPageRoute.js'))(names);
             fs.outputFileSync(routeFilePath, routeFileData);
             spinner.succeed(chalk.green('子页面路由创建成功'));
+
+            // 创建子页面接口
+            let apiFilePath = path.join(currentDirectory, 'api.js');
+            let apiFileData = _.template(require('../../.yipack/template/pageApi.js'))(rootNames);
+            fs.outputFileSync(apiFilePath, apiFileData);
+            spinner.succeed(chalk.green('子页面接口创建成功'));
         } else {
             spinner.warn(chalk.green('子页面已存在'));
         }
@@ -67,16 +88,23 @@ module.exports = async function newPage(cmd) {
         // 如果需要创建子组件
         if (cmd.comp) {
             // 创建子页面组件目录
-            let compDirPath = path.join(pageDirPath, 'subPages', subNames.camelCaseName, 'comps');
+            let compDirPath = path.join(currentDirectory, 'comps');
             if (fs.existsSync(compDirPath) === false) {
                 fs.ensureDirSync(compDirPath);
             }
             // 创建组件
             let htmlFilePath = path.join(compDirPath, compNames.camelCaseName + '.vue');
             if (fs.existsSync(htmlFilePath) === false) {
+                // 创建子页面组件
                 let htmlFileData = _.template(require('../../.yipack/template/subPageCompHtml.js'))(names);
                 fs.outputFileSync(htmlFilePath, htmlFileData);
                 spinner.succeed(chalk.green('子页面组件创建成功'));
+
+                // 创建子页面组件说明
+                let readmeFilePath = path.join(compDirPath, 'readme.md');
+                let readmeFileData = _.template(require('../../.yipack/template/readme.js'))(rootNames);
+                fs.outputFileSync(readmeFilePath, readmeFileData);
+                spinner.succeed(chalk.green('子页面组件说明创建成功'));
             } else {
                 spinner.warn(chalk.green('子页面组件已存在'));
             }
@@ -92,17 +120,32 @@ module.exports = async function newPage(cmd) {
         }
         let subNames = tool.getNames(cmd.subView);
         let names = tool.getAllNames(rootNames, subNames, compNames);
+        let currentDirectory = path.join(pageDirPath, 'subViews', subNames.camelCaseName);
         // 创建二级页面
-        let htmlFilePath = path.join(pageDirPath, 'subViews', subNames.camelCaseName, 'index.vue');
+        let htmlFilePath = path.join(currentDirectory, 'index.vue');
         if (fs.existsSync(htmlFilePath) === false) {
+            // 创建子视图
             let htmlFileData = _.template(require('../../.yipack/template/subViewHtml.js'))(names);
             fs.outputFileSync(htmlFilePath, htmlFileData);
             spinner.succeed(chalk.green('子视图创建成功'));
-            // 创建二级路由
-            let routeFilePath = path.join(pageDirPath, 'subViews', subNames.camelCaseName, 'routeView.js');
+
+            // 创建子视图说明
+            let readmeFilePath = path.join(currentDirectory, 'readme.md');
+            let readmeFileData = _.template(require('../../.yipack/template/readme.js'))(rootNames);
+            fs.outputFileSync(readmeFilePath, readmeFileData);
+            spinner.succeed(chalk.green('子视图说明创建成功'));
+
+            // 创建子视图路由
+            let routeFilePath = path.join(currentDirectory, 'routeView.js');
             let routeFileData = _.template(require('../../.yipack/template/subViewRoute.js'))(names);
             fs.outputFileSync(routeFilePath, routeFileData);
             spinner.succeed(chalk.green('子视图路由创建成功'));
+
+            // 创建子视图接口
+            let apiFilePath = path.join(currentDirectory, 'api.js');
+            let apiFileData = _.template(require('../../.yipack/template/pageApi.js'))(rootNames);
+            fs.outputFileSync(apiFilePath, apiFileData);
+            spinner.succeed(chalk.green('子视图接口创建成功'));
         } else {
             spinner.warn(chalk.green('子视图已存在'));
         }
@@ -110,16 +153,24 @@ module.exports = async function newPage(cmd) {
         // 如果需要创建子组件
         if (cmd.comp) {
             // 创建子页面组件目录
-            let compDirPath = path.join(pageDirPath, 'subViews', subNames.camelCaseName, 'comps');
+            let compDirPath = path.join(currentDirectory, 'comps');
+            // 确保子视图组件目录存在
             if (fs.existsSync(compDirPath) === false) {
                 fs.ensureDirSync(compDirPath);
             }
-            // 创建组件
+
             let htmlFilePath = path.join(compDirPath, compNames.camelCaseName + '.vue');
             if (fs.existsSync(htmlFilePath) === false) {
+                // 创建子视图组件
                 let htmlFileData = _.template(require('../../.yipack/template/subViewCompHtml.js'))(names);
                 fs.outputFileSync(htmlFilePath, htmlFileData);
                 spinner.succeed(chalk.green('子视图组件创建成功'));
+
+                // 创建子页面组件说明
+                let readmeFilePath = path.join(compDirPath, 'readme.md');
+                let readmeFileData = _.template(require('../../.yipack/template/readme.js'))(rootNames);
+                fs.outputFileSync(readmeFilePath, readmeFileData);
+                spinner.succeed(chalk.green('子视图组件说明创建成功'));
             } else {
                 spinner.warn(chalk.green('子视图组件已存在'));
             }
