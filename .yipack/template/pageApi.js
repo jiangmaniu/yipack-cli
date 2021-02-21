7d74b6b77e6648d377bcd5cdc4c43e7973494208
chensuiyi <chenbimo@gmail.com>
2021-02-21 22:32:05 +0800

    refactor: 默认api模板提供增删改查三个接口

--- .yipack/template/pageApi.js
+++ .yipack/template/pageApi.js
@@ -1,11 +1,35 @@
 module.exports = `
 import request from '@src/request/api.js';
 export default {
-    // 接口示例
-    test(params) {
+    // 添加
+    ins(params) {
         return request({
             method: 'post',
-            url: '/test',
+            url: '/ins',
+            params: params
+        });
+    },
+    // 删除
+    del(params) {
+        return request({
+            method: 'post',
+            url: '/del',
+            params: params
+        });
+    },
+    // 更新
+    upd(params) {
+        return request({
+            method: 'post',
+            url: '/upd',
+            params: params
+        });
+    },
+    // 查询
+    sel(params) {
+        return request({
+            method: 'post',
+            url: '/sel',
             params: params
         });
     }
