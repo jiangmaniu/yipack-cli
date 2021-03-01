9317f5981b3f8645c42b91851a46d1d574d6e9e1
chensuiyi <chenbimo@gmail.com>
2021-03-01 15:15:24 +0800

    feat: 增加组件的属性排序规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,47 @@ module.exports = {
     rules: {
         'no-unused-vars': 'off',
         'no-var': 'warn',
-        eqeqeq: 'warn'
+        eqeqeq: 'warn',
+        'vue/order-in-components': [
+            'warn',
+            {
+                order: [
+                    //
+                    'el',
+                    'name',
+                    'key',
+                    'parent',
+                    'functional',
+                    ['delimiters', 'comments'],
+                    ['components', 'directives', 'filters'],
+                    'extends',
+                    'mixins',
+                    ['provide', 'inject'],
+                    'ROUTER_GUARDS',
+                    'layout',
+                    'middleware',
+                    'validate',
+                    'scrollToTop',
+                    'transition',
+                    'loading',
+                    'inheritAttrs',
+                    'model',
+                    ['props', 'propsData'],
+                    'emits',
+                    'setup',
+                    'asyncData',
+                    'data',
+                    'fetch',
+                    'head',
+                    'computed',
+                    'watch',
+                    'watchQuery',
+                    'LIFECYCLE_HOOKS',
+                    'methods',
+                    ['template', 'render'],
+                    'renderError'
+                ]
+            }
+        ]
     }
 };
