1a486da69a419cc817ecd531ee8b2ce0548e6af3
chensuiyi <chenbimo@gmail.com>
2021-03-10 04:40:10 +0800

    fix: 修复页面路由的接口生成异常的打印变量错误问题

--- .yipack/template/pageHtml.js
+++ .yipack/template/pageHtml.js
@@ -17,54 +17,70 @@ export default {
     methods:{
         // 页面初始化操作
         on_init() {},
-        // 接口-增加
+        // 接口 - 增加
         api_insert() {
             return new Promise((resolve, reject) => {
                 this.$Apis.<%= startCaseNameRouteDot %>
                     .insert()
                     .then((res) => {
+                        console.log('api_insert(res): 接口描述');
+                        console.dir(res);
                         resolve(res);
                     })
                     .catch((err) => {
+                        console.log('api_insert(err): 接口描述');
+                        console.dir(err);
                         reject(err);
                     });
             });
         },
-        // 接口-删除
+        // 接口 - 删除
         api_delete() {
             return new Promise((resolve, reject) => {
                 this.$Apis.<%= startCaseNameRouteDot %>
                     .delete()
                     .then((res) => {
+                        console.log('api_delete(res): 接口描述');
+                        console.dir(res);
                         resolve(res);
                     })
                     .catch((err) => {
+                        console.log('api_delete(err): 接口描述');
+                        console.dir(err);
                         reject(err);
                     });
             });
         },
-        // 接口-修改
+        // 接口 - 修改
         api_update() {
             return new Promise((resolve, reject) => {
                 this.$Apis.<%= startCaseNameRouteDot %>
                     .update()
                     .then((res) => {
+                        console.log('api_update(res): 接口描述');
+                        console.dir(res);
                         resolve(res);
                     })
                     .catch((err) => {
+                        console.log('api_update(err): 接口描述');
+                        console.dir(err);
                         reject(err);
                     });
             });
         },
-        // 接口-查询
+        // 接口 - 查询
         api_select() {
             return new Promise((resolve, reject) => {
                 this.$Apis.<%= startCaseNameRouteDot %>
                     .select()
                     .then((res) => {
+                        console.log('api_select(res): 接口描述');
+                        console.dir(res);
                         resolve(res);
                     })
                     .catch((err) => {
+                        console.log('api_select(res): 接口描述');
+                        console.dir(err)
                         reject(err);
                     });
             });
